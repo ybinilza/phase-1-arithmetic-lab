b1a9b83d97b28370abe70c7821eb63fbaf28b596
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-30 01:19:05 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1, num2 , multiply;
+
+num1=31;
+num2=2;
+multiply=num1*num2;
+
+
+let random = Math.floor((Math.random()*10)+1);
+let num3=22;
+let num4=6;
+let mod= Math.floor(num3%num4);
+let max = Math.max(1,2,3,4,5,20);
+
